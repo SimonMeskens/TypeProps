aa310d93da470eff4e6db196167b4e5cf685546d
Simon Meskens <meskens.simon@gmail.com>
2018-05-07 16:47:27 +0200

    New matching algorithm

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,42 @@ declare module "typeprops" {
     // Waiting for proper top type
     type unknown = {} | null | undefined;
 
-    interface TypeProps<T = {}, Params extends ArrayLike<any> = never> {
+    // TODO: TS 2.9: add symbol support
+    interface TypeProp {
+        infer: ArrayLike<unknown>;
+        construct: unknown;
+    }
+
+    /**
+     * A dictionary of 'TypeProps' that can be extended through [declaration merging](https://www.typescriptlang.org/docs/handbook/declaration-merging.html#merging-interfaces).
+     *
+     * 'T' is the type you want to check and/or mutate.
+     * 'Params' is a list of type parameters to mutate 'T' with
+     *
+     * Each entry should have a unique key, typed with the following two properties:
+     *
+     * ```TypeScript
+     * {
+     *     infer: T extends MyType<infer A> ? [A] : never;
+     *     construct: MyType<Params[0]>
+     * }
+     * ```
+     *
+     * 'infer' should always verify the type is correct and return 'never' otherwise. It should return an ArrayLike with a list of inferred type parameters.
+     *
+     * 'construct' should return the mutated type using the passed in 'Params'. It should generally not check if the passed in type 'T' is correct. If you want to reuse parameters from 'T', you can do it like this:
+     *
+     * ```TypeScript
+     * {
+     *     infer: T extends MyType<any, infer A> ? [A] : never;
+     *     construct: MyType<Parameters<T>[0], Params[0]>
+     * }
+     * ```
+     */
+    interface TypeProps<T, Params extends ArrayLike<any>> {
         array: {
-            infer: T extends Array<infer A> ? [A] : never;
-            construct: Params[0][];
+            infer: T extends Array<infer A> ? T : never;
+            construct: Params[number][];
         };
         null: {
             infer: null extends T ? [never] : never;
@@ -21,59 +53,74 @@ declare module "typeprops" {
         };
     }
 
-    type Match<T> = T extends infer U
-        ? ({} extends U
+    /**
+     * A type-level pattern-matcher.
+     *
+     * 'T' is the type to match against
+     * 'Params' is the parameters you want to mutate T with
+     * 'Override' lets you optionally define overrides for the type dictionary
+     *
+     * A rough overview of the algorithm:
+     * - Split union type into distinct cases using a distributive conditional
+     * - Construct a type dictionary using TypeProps & Override
+     * - If T is '{}', set the indexer to 'any'
+     * TODO: make Match use Override in the indexer
+     * TODO: finish explaining algorithm
+     */
+    type Match<
+        T,
+        Params extends ArrayLike<any> = ArrayLike<any>,
+        Override extends { [K in keyof Override]: TypeProp } = never
+    > = T extends infer U
+        ? Dictionary<U, Params, Override>[({} extends U
               ? any
-              : TypeProps<U>[Exclude<
-                    keyof TypeProps,
+              : Dictionary<U, Params, Override>[Exclude<
+                    keyof Dictionary<U, Params, Override>,
                     "unfound"
                 >]["infer"]) extends never
-            ? "unfound"
-            : {
-                  [Key in Exclude<keyof TypeProps, "unfound">]: TypeProps<
-                      T
-                  >[Key]["infer"] extends never
-                      ? never
-                      : Key
-              }[Exclude<keyof TypeProps, "unfound">]
+              ? "unfound"
+              : {
+                    [Key in Exclude<
+                        keyof Dictionary<U, Params, Override>,
+                        "unfound"
+                    >]: Dictionary<
+                        U,
+                        Params,
+                        Override
+                    >[Key]["infer"] extends never
+                        ? never
+                        : Key
+                }[Exclude<keyof Dictionary<U, Params, Override>, "unfound">]]
         : never;
 
     type Parameters<
         T,
-        Overwrite extends {
-            [K in keyof Overwrite]: {
-                infer: ArrayLike<any>;
-                construct: unknown;
-            }
-        } = {
-            [K in keyof Overwrite]: {
-                infer: ArrayLike<any>;
-                construct: unknown;
-            }
-        },
-        Dictionary extends {
-            [K in keyof Dictionary]: {
-                infer: ArrayLike<any>;
-                construct: unknown;
-            }
-        } = Overwrite extends never
-            ? TypeProps<T>
-            : Combine<TypeProps<T>, Overwrite>
-    > = Dictionary[Match<T>]["infer"];
+        Override extends { [K in keyof Override]: TypeProp } = never
+    > = Match<T, never, Override>["infer"];
+
+    type Parameter<
+        T,
+        Index extends number = 0,
+        Override extends { [K in keyof Override]: TypeProp } = never
+    > = Parameters<T>[Index];
 
     type Generic<
         T,
         Params extends ArrayLike<any> = ArrayLike<any>,
-        Overwrite extends {
-            [K in keyof Overwrite]: { infer: unknown; construct: unknown }
-        } = { [K in keyof Overwrite]: { infer: unknown; construct: unknown } }
-    > = Overwrite extends never
-        ? TypeProps<T, Params>[Match<T>]["construct"]
-        : Combine<TypeProps<T, Params>, Overwrite>[Match<T>]["construct"];
+        Override extends { [K in keyof Override]: TypeProp } = never
+    > = Match<T, Params, Override>["construct"];
 
-    type Combine<T, U> = {
-        [K in keyof (T & U)]: K extends keyof U
-            ? U[K]
-            : K extends keyof T ? T[K] : never
+    type Dictionary<
+        T,
+        Params extends ArrayLike<any>,
+        Override extends { [K in keyof Override]: TypeProp } = never
+    > = {
+        [K in
+            | (keyof TypeProps<T, Params>)
+            | keyof Override]: K extends keyof Override
+            ? Override[K]
+            : K extends keyof TypeProps<T, Params>
+                ? TypeProps<T, Params>[K]
+                : never
     };
 }
