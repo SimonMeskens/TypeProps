a43c285729fca0e787e2265e5243e9c99cc5cce4
Simon Meskens <meskens.simon@gmail.com>
2018-05-09 11:07:35 +0200

    Split Match
    for better readability

--- match.ts
+++ match.ts
@@ -7,50 +7,48 @@ import { TypeProp } from ".";
  * 'T' is the type to match against
  * 'Params' is the parameters you want to mutate T with
  * 'Override' lets you optionally define overrides for the type dictionary
- *
- * A rough overview of the algorithm:
- * - Split union type into distinct cases using a distributive conditional
- * - Construct a type dictionary using TypeProps & Override
- * - If T is '{}', set the checking type to 'any'
- * - Otherwise, set the checking type to the 'infer' property of T's TypeProp
- * - If the checking type is 'never', set the indexer to "unfound"
- * - Otherwise, use a mapped type to get the unique key for T's TypeProp, excluding "unfound" and set it as the indexer
- * - If the checking type was 'any', it will run both previous paths and return a union of "unfound" and all other unique keys as indexer
- * - Index the type dictionary with the indexer and return the TypeProp
  */
 export type Match<
     T,
     Params extends ArrayLike<any> = ArrayLike<any>,
     Override extends { [K in keyof Override]: TypeProp } = never
-> = T extends infer U
-    ? Dictionary<U, Params, Override>[({} extends U
-          ? any
-          : TypeProps<U, Params>[Exclude<
-                keyof TypeProps<U, Params>,
-                "unfound"
-            >]["infer"]) extends never
-          ? "unfound"
-          : {
-                [Key in Exclude<
-                    keyof TypeProps<U, Params>,
-                    "unfound"
-                >]: TypeProps<U, Params>[Key]["infer"] extends never
-                    ? never
-                    : Key
-            }[Exclude<keyof TypeProps<U, Params>, "unfound">]]
-    : never;
+> =
+    // Check each type of union separately, using [distributive conditional types](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#distributive-conditional-types)
+    T extends infer U
+        ? Dictionary<U, Params, Override>[CheckMatch<T, Params>]
+        : never;
 
-/**
- * Construct a type dictionary
- *
- * 'T' is the type to mutate
- * 'Params' is the array of type parameters to mutate into
- * 'Override' optionally allows you to override select types in the type dictionary
- */
-export type Dictionary<
+// If a match, return unique key
+type CheckMatch<T, Params extends ArrayLike<any> = ArrayLike<any>> =
+    // Handle the "unfound" case
+    // If T is `{}` or `any`, infer `any`, this will select both paths of the conditional and return `"unfound" | KeyOf<T, Params>`
+    ({} extends T ? any : Infer<T, Params>) extends never
+        ? "unfound"
+        : KeyOf<T, Params>;
+
+// Match for all keys and return key if matched, then construct union
+type KeyOf<T, Params extends ArrayLike<any> = ArrayLike<any>> = {
+    [Key in Keys<T, Params>]: Infer<T, Params, Key> extends never ? never : Key
+}[Keys<T, Params>];
+
+// List of TypeProps keys, excluding "unfound"
+type Keys<T, Params extends ArrayLike<any>> = Exclude<
+    keyof TypeProps<T, Params>,
+    "unfound"
+>;
+
+// Core matching algorithm
+type Infer<
     T,
     Params extends ArrayLike<any>,
-    Override extends { [K in keyof Override]: TypeProp } = never
+    Key extends Keys<T, Params> = Keys<T, Params>
+> = TypeProps<T, Params>[Key]["infer"];
+
+// Combine TypeProps and Override into single dictionary
+type Dictionary<
+    T,
+    Params extends ArrayLike<any>,
+    Override extends { [K in keyof Override]: TypeProp }
 > = {
     [K in
         | (keyof TypeProps<T, Params>)
